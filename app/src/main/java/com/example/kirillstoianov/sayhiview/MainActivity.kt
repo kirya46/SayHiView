03e441397bc3138b26127f29aba8a1667dd25e62
kirill.stoianov <kirillstoianov46@gmail.com>
2018-11-09 14:41:48 +0200

    Added:drawing title text Added: gradient background for view.

--- app/src/main/java/com/example/kirillstoianov/sayhiview/MainActivity.kt
+++ app/src/main/java/com/example/kirillstoianov/sayhiview/MainActivity.kt
@@ -13,8 +13,8 @@ class MainActivity : AppCompatActivity() {
         val sayHiView = SayHiView(this@MainActivity)
 //        val layoutParams =ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
 //        sayHiView.layoutParams = layoutParams
-//        sayHiView.layoutParams.width = 300
-//        sayHiView.layoutParams.height = 700
+//        sayHiView.layoutParams.width = 664
+//        sayHiView.layoutParams.height = 930
         container.addView(sayHiView)
 
         sayHiView.post {
--- app/src/main/java/com/example/kirillstoianov/sayhiview/SayHiView.kt
+++ app/src/main/java/com/example/kirillstoianov/sayhiview/SayHiView.kt
@@ -3,74 +3,170 @@ package com.example.kirillstoianov.sayhiview
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
+import android.util.TypedValue
 import android.view.View
 import kotlin.math.roundToInt
 
 
 /**
  * Created by Kirill Stoianov on 07.11.18.
- * http://android-er.blogspot.com/2014/05/draw-star-on-canvas.html
- * http://android-er.blogspot.com/2014/05/draw-path-of-polygon-on-canvas-of.html
+ *
+ * @link http://android-er.blogspot.com/2014/05/draw-star-on-canvas.html
+ * @link http://android-er.blogspot.com/2014/05/draw-path-of-polygon-on-canvas-of.html
+ * @link https://stackoverflow.com/questions/3630086/how-to-get-string-width-on-android
  */
 class SayHiView(context: Context) : View(context) {
 
+    //CONFETTI
+    /**
+     * List with confetti shapes.
+     */
     private var confettiItems: ArrayList<ConfettiShape> = ArrayList()
-    private val colors: ArrayList<Int> = arrayListOf(
+
+    /**
+     * List with colors for confetti shapes.
+     */
+    private val confettiColors: ArrayList<Int> = arrayListOf(
         Color.parseColor("#ff0096"), Color.parseColor("#fe692e"),
         Color.parseColor("#50e3c2"), Color.parseColor("#ffb300"),
         Color.parseColor("#ff415c"), Color.parseColor("#ff415c"),
         Color.parseColor("#04d95c"), Color.parseColor("#135bfe")
     )
 
+    //HAND
+    /**
+     * Bitmap with hand.
+     */
     private val handBitmap: Bitmap by lazy {
         val source = BitmapFactory.decodeResource(resources, R.drawable.img_sayhi_hand)
-        val bitmapWidth = getBitmapWidth()
-        val bitmapHeight = getBitmapHeight()
+        val bitmapWidth = getHandBitmapWidth()
+        val bitmapHeight = getHandBitmapHeight()
         return@lazy Bitmap.createScaledBitmap(source, bitmapWidth, bitmapHeight, true)
     }
 
-    private fun getBitmapWidth(): Int = Math.min(width, height) / 3
-    private fun getBitmapHeight(): Int {
-        val ratio = 1.3
-        return (Math.min(width, height) / 3 * ratio).roundToInt()
+    /**
+     * Value which need multiply on [getHandBitmapWidth]
+     * for get [handBitmap] height.
+     */
+    private val handBitmapHeightRatio = 1.3f
+
+
+    //TITLE
+    /**
+     * Title text.
+     */
+    private val titleText: String = "You said “Hi!”"
+
+    /**
+     * Title text bounds for calculate
+     * text position on canvas.
+     */
+    private val titleTextBound = Rect()
+
+    /**
+     * Title text paint.
+     */
+    private val titleTextPaint by lazy {
+        Paint().apply {
+            color = Color.WHITE
+            typeface = Typeface.create("sans-serif-black", Typeface.NORMAL)
+            textSize = height / 12f
+
+            val shadowRadiusDpSize = 4
+            val scaledShadowSizeInPx = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                shadowRadiusDpSize.toFloat(),
+                context.resources.displayMetrics
+            )
+            setShadowLayer(scaledShadowSizeInPx, 0f, 4f, Color.parseColor("#80000000"))
+        }
     }
 
-    //Animated values
-    private var animateRadius: Float = 0f
-    private var handDegree: Float = 0f
-    private var handAnimatedWidth: Float = 0f
+    //BACKGROUND
+    /**
+     * Background paint for draw gradient on canvas.
+     */
+    private val backgroundPaint by lazy {
+        Paint().apply {
+            val linearGradient = LinearGradient(
+                0f, 0f,
+                0f, height.toFloat(),
+                Color.parseColor("#00000000"), Color.parseColor("#80000000"),
+                Shader.TileMode.CLAMP
+            )
+            shader = linearGradient
+            isDither = true
+        }
+    }
 
-    val handMatrix = Matrix()
+    //ANIMATED VALUES
+    /**
+     * Radius of imaginary circle
+     * which increase with [confettiDistanceAnimator].
+     *
+     * Need for animate [ConfettiShape]'s transition from center
+     * to destination position.
+     */
+    private var animatedConfettiRadius: Float = 0f
 
+    /**
+     * Rotate degree anim value if [handBitmap].
+     *
+     * Need for animate 'say hi' gesture.
+     */
+    private var animatedHandDegree: Float = 0f
 
+    /**
+     * Scale anim value of [handBitmap].
+     *
+     * Need for animate [handBitmap] showing effect.
+     */
+    private var animatedHandWidth: Float = 0f
+
+    /**
+     * Matrix for draw [handBitmap] on [Canvas]
+     * with: 'translate','rotate' and 'scale' effects.
+     */
+    private val handMatrix = Matrix()
+
+    /**
+     * Animator of [animatedConfettiRadius] for animate [ConfettiShape]'s
+     * transition effect.
+     */
     private val confettiDistanceAnimator by lazy {
-        ValueAnimator.ofFloat(0f/*Math.min(width, height).toFloat() / 4*/, Math.min(width, height).toFloat() / 2.5f)
+        ValueAnimator.ofFloat(0f, Math.min(width, height).toFloat() / 2.5f)
             .apply {
                 duration = 400
                 addUpdateListener {
-                    animateRadius = it.animatedValue as Float
+                    animatedConfettiRadius = it.animatedValue as Float
                     invalidate()
                 }
             }
     }
 
+    /**
+     * Animator of [handBitmap] rotation.
+     */
     private val handDegreeAnimator by lazy {
         ValueAnimator.ofFloat(-15f, 15f).apply {
             duration = 500
             repeatCount = ValueAnimator.INFINITE
             repeatMode = ValueAnimator.REVERSE
             addUpdateListener {
-                handDegree = it.animatedValue as Float
+                animatedHandDegree = it.animatedValue as Float
                 invalidate()
             }
         }
     }
 
+    /**
+     * Animator of [handBitmap] scaling effetc.
+     */
     private val handScaleAnimator by lazy {
-        ValueAnimator.ofFloat(0f, getBitmapWidth().toFloat()).apply {
+        ValueAnimator.ofFloat(0f, getHandBitmapWidth().toFloat()).apply {
             startDelay = 350
             duration = 250
-            addUpdateListener { handAnimatedWidth = it.animatedValue as Float }
+            addUpdateListener { animatedHandWidth = it.animatedValue as Float }
         }
     }
 
@@ -88,6 +184,9 @@ class SayHiView(context: Context) : View(context) {
 
         canvas?.apply {
 
+            //draw gradient background
+            drawGradientBackground(this)
+
             //draw confetti items
             confettiItems.forEach { shape ->
                 drawConfetti(this, shape)
@@ -101,40 +200,69 @@ class SayHiView(context: Context) : View(context) {
         }
     }
 
-    private fun drawTitle(canvas: Canvas) {
-        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-
-        //https://stackoverflow.com/questions/3630086/how-to-get-string-width-on-android
-
-//        val bounds = Rect()
-//        val textPaint = textView.getPaint()
-//        textPaint.getTextBounds(text, 0, text.length(), bounds)
-//        val height = bounds.height()
-//        val width = bounds.width()
-
-        canvas.drawText("You said “Hi!”", width / 2f, height / 5f, Paint().apply {
-            color = Color.BLACK
-            textSize = 150f
-        })
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        //reset resources
+        handBitmap.recycle()
+        confettiItems.clear()
+        matrix.reset()
     }
 
+    /**
+     * Start view animation.
+     */
     fun startAnimate() {
         handScaleAnimator.start()
         confettiDistanceAnimator.start()
         handDegreeAnimator.start()
     }
 
-    override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
-        handBitmap.recycle()
+    /**
+     * Draw view gradient background.
+     */
+    private fun drawGradientBackground(canvas: Canvas) {
+        canvas.drawPaint(backgroundPaint)
     }
 
+    /**
+     * Draw confetti shape.
+     *
+     */
+    private fun drawConfetti(canvas: Canvas, shape: ConfettiShape) {
+        shape.pX = width / 2 +
+                ((animatedConfettiRadius + shape.radiusOffset) * Math.cos(Math.toRadians(shape.angle.toDouble())).toFloat())
+        shape.pY = height / 2 +
+                ((animatedConfettiRadius + shape.radiusOffset) * Math.sin(Math.toRadians(shape.angle.toDouble())).toFloat())
+        shape.radius = getShapeRadius(shape)
+
+        shape.draw(canvas)
+    }
+
+    /**
+     * Draw view title text.
+     */
+    private fun drawTitle(canvas: Canvas) {
+
+        titleTextPaint.getTextBounds(titleText, 0, titleText.length, titleTextBound)
+
+        val textWidth = titleTextBound.width()
+        val textHeight = titleTextBound.height()
+
+
+        val textLeft = width / 2f - (textWidth / 2)
+        val textTop = height / 5f //- (textHeight / 2)
+        canvas.drawText(titleText, textLeft, textTop, titleTextPaint)
+    }
+
+    /**
+     * Draw [handBitmap].
+     */
     private fun drawHand(canvas: Canvas) {
 
         handMatrix.reset()
 
-        val scaleWidthFactor = handAnimatedWidth / handBitmap.width
-        val scaleHeightFactor = handAnimatedWidth * 1.3f / handBitmap.height
+        val scaleWidthFactor = animatedHandWidth / handBitmap.width
+        val scaleHeightFactor = animatedHandWidth * handBitmapHeightRatio / handBitmap.height
 
         val newWidth = handBitmap.width * scaleWidthFactor
         val newHeight = handBitmap.height * scaleHeightFactor
@@ -142,23 +270,22 @@ class SayHiView(context: Context) : View(context) {
         val handLeft = (width / 2f) - newWidth / 2
         val handTop = (height / 2f) - newHeight / 2
 
-        handMatrix.preRotate(handDegree, newWidth / 2f, newHeight)
+        handMatrix.preRotate(
+            animatedHandDegree,
+            newWidth / 2f/*point of rotation by x-asix*/,
+            newHeight/*point of rotate by y-axis*/
+        )
         handMatrix.postScale(scaleWidthFactor, scaleHeightFactor)
         handMatrix.postTranslate(handLeft, handTop)
 
         canvas.drawBitmap(handBitmap, handMatrix, null)
     }
 
-    private fun drawConfetti(canvas: Canvas, shape: ConfettiShape) {
-        shape.pX = width / 2 +
-                ((animateRadius + shape.radiusOffset) * Math.cos(Math.toRadians(shape.angle.toDouble())).toFloat())
-        shape.pY = height / 2 +
-                ((animateRadius + shape.radiusOffset) * Math.sin(Math.toRadians(shape.angle.toDouble())).toFloat())
-        shape.radius = getShapeRadius(shape)
-
-        shape.draw(canvas)
-    }
-
+    /**
+     * Get shape radius for drawing.
+     *
+     * [SayHiView] support 3 different size's for [ConfettiShape].
+     */
     private fun getShapeRadius(confettiShape: ConfettiShape): Float {
         val diameter = when (confettiShape.size) {
             ConfettiShape.Size.SMALL -> Math.min(width, height) / 36f
@@ -168,12 +295,18 @@ class SayHiView(context: Context) : View(context) {
         return diameter / 2
     }
 
+    /**
+     * Fill local list with random [ConfettiShape]'s.
+     */
     private fun generateConfetti() {
         IntRange(0, 30).forEach {
             confettiItems.add(getRandomShape())
         }
     }
 
+    /**
+     * Get random [ConfettiShape] item with random configuration.
+     */
     private fun getRandomShape(): ConfettiShape {
         val confettiShape = ConfettiShape(getRandomShapeType())
         confettiShape.angle = getRandomDegree().toFloat()
@@ -183,12 +316,29 @@ class SayHiView(context: Context) : View(context) {
         return confettiShape
     }
 
+    /**
+     * Get random degree for [ConfettiShape]
+     * on imaginary circle.
+     *
+     * @return - values in range 0.. 360.
+     */
     private fun getRandomDegree(): Int {
         return (0 until 180).random() * 2
     }
 
-    private fun getRandomRadiusOffset(): Int = (0 until (width / 5)).random()
+    /**
+     * Get random value which wil attach to
+     * [animatedConfettiRadius] for make different radius
+     * of imaginary circle for each [ConfettiShape].
+     */
+    private fun getRandomRadiusOffset(): Int {
+        if (width == 0) return 0
+        return (0 until (width / 5)).random()
+    }
 
+    /**
+     * Get random shape size.
+     */
     private fun getRandomShapeSize(): ConfettiShape.Size {
         val random = (0 until 4).random()
         return when (random) {
@@ -199,11 +349,17 @@ class SayHiView(context: Context) : View(context) {
         }
     }
 
+    /**
+     * Get random color for each [ConfettiShape].
+     */
     private fun getRandomColor(): Int {
-        val random = (0 until colors.size).random()
-        return colors[random]
+        val random = (0 until confettiColors.size).random()
+        return confettiColors[random]
     }
 
+    /**
+     * Get random shape type.
+     */
     private fun getRandomShapeType(): ConfettiShape.Type {
         val random = (0 until 5).random()
         return when (random) {
@@ -215,4 +371,19 @@ class SayHiView(context: Context) : View(context) {
         }
     }
 
+    /**
+     * Get [handBitmap] width.
+     */
+    private fun getHandBitmapWidth(): Int {
+        if (width == 0 || height == 0) return 0
+        return Math.min(width, height) / 3
+    }
+
+    /**
+     * Get [handBitmap] height.
+     */
+    private fun getHandBitmapHeight(): Int {
+        if (width == 0 || height == 0) return 0
+        return (getHandBitmapWidth() * handBitmapHeightRatio).roundToInt()
+    }
 }
